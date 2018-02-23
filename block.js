85e7490ec4ab264b212e1b9e335ff63d89939a8e
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-24 00:04:42 +0100

    my first block with unit testing with jest

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -13,6 +15,26 @@ class Block {
       Hash     : ${this.hash.substring(0, 10)}
       Data     : ${this.data}
     `;
+  }
+
+  // using static we make enable to be this function used if this
+  // module is exported to other files => genesis() is accesible
+  // in other words, we don't need to creacte Block instances to
+  // use this funcion => retroalimentation of class ES6
+  static genesis() {
+    return new this('Genesis time', '-----', 'f15r7-h45h', []);
+  }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
